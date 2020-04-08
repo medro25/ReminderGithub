75c5cd4e47da5957852c5a0f2a4d330ce337ed3c
errafay <errafayamine@gmail.com>
2020-04-08 14:18:12 +0200

    AlarmManager, BroadcastReceiver

--- app/src/main/java/com/example/reminder2020/MainActivity.kt
+++ app/src/main/java/com/example/reminder2020/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,22 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var fabOpened= false
+        var fabOpened = false
 
         fab.setOnClickListener {
 
             if (!fabOpened) {
 
-                fabOpened =true
+                fabOpened = true
 
                 fab_map.animate().translationY(-resources.getDimension(R.dimen.standard_66))
                 fab_time.animate().translationY(-resources.getDimension(R.dimen.standard_166))
 
 
-
             } else {
 
-                fabOpened =false
+                fabOpened = false
                 fab_map.animate().translationY(0f)
                 fab_time.animate().translationY(0f)
 
@@ -40,22 +43,52 @@ class MainActivity : AppCompatActivity() {
 
         fab_time.setOnClickListener {
 
-            val intent = Intent(applicationContext ,TimeActivity::class.java)
+            val intent = Intent(applicationContext, TimeActivity::class.java)
             startActivity(intent)
 
         }
 
         fab_map.setOnClickListener {
 
-            val intent=Intent(applicationContext ,MapActivity::class.java)
+            val intent = Intent(applicationContext, MapActivity::class.java)
             startActivity(intent)
         }
 
-        val data = arrayOf("Oulu" ,"Helsinki", "Tampere")
 
-        val reminderAdapter =ReminderAdapter(applicationContext,data)
-         list.adapter = reminderAdapter
 
-        }
+
+
+
     }
 
+    override fun onResume() {
+        super.onResume()
+refrechlist ()
+
+
+    }
+
+    private fun refrechlist() {
+
+        doAsync {
+            val db = Room.databaseBuilder(
+                applicationContext, AppDatabase::class.java, "reminder"
+            ).build()
+            val reminders = db.reminderDao().getReminders()
+            db.close()
+
+            uiThread {
+                if (reminders.isNotEmpty()) {
+                    val adapter = ReminderAdapter(applicationContext, reminders)
+                    list.adapter = adapter
+                } else {
+
+                    toast("No reminder yet")
+                }
+            }
+
+
+        }
+
+    }
+}
